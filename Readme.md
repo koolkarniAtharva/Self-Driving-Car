9a6ca90b906c72118bcf9bf26200c44e9fb7cb7a
ATHARVA KULKARNI <152779278+koolkarniAtharva@users.noreply.github.com>
2025-10-13 16:54:22 +0530

    Add road definition and lane system

---
+++ Readme.md
@@ -0,0 +1,1 @@
+# https://github.com/gniziemazity/self-driving-car
--- controls.js
+++ controls.js
@@ -1,47 +1,45 @@
 class Controls{
     constructor(){
-        this.forward =  false;
-        this.left =     false;
-        this.right =    false;
-        this.reverse =  false;
+        this.forward=false;
+        this.left=false;
+        this.right=false;
+        this.reverse=false;
 
-        this.addKeyboardListeners();
+        this.#addKeyboardListeners();
     }
 
-    addKeyboardListeners(){
+    #addKeyboardListeners(){
         document.onkeydown=(event)=>{
             switch(event.key){
-                case"ArrowLeft":
+                case "ArrowLeft":
                     this.left=true;
                     break;
-                case"ArrowRight":
+                case "ArrowRight":
                     this.right=true;
                     break;
-                case"ArrowUp":
+                case "ArrowUp":
                     this.forward=true;
                     break;
-                case"ArrowDown":
+                case "ArrowDown":
                     this.reverse=true;
                     break;
-                }
-                
+            }
         }
         document.onkeyup=(event)=>{
             switch(event.key){
-                case"ArrowLeft":
+                case "ArrowLeft":
                     this.left=false;
                     break;
-                case"ArrowRight":
+                case "ArrowRight":
                     this.right=false;
-                    break;  
-                case"ArrowUp":
+                    break;
+                case "ArrowUp":
                     this.forward=false;
                     break;
-                case"ArrowDown":
+                case "ArrowDown":
                     this.reverse=false;
                     break;
-                }
-                
+            }
         }
     }
 }
